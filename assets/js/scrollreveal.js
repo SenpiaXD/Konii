b11bd179a4ff0bca87d7ea6008d0d66223757c88
Koni <145802334+SenpiaXD@users.noreply.github.com>
2023-10-16 09:29:57 +0800

    ...

---
+++ assets/js/scrollreveal.js
@@ -0,0 +1,35 @@
+//Revealing Animation when the user scrolls/open/refresh
+ScrollReveal({ reset: true });
+
+
+ScrollReveal().reveal(".fade-in", {
+    duration: 2500,
+    move: 0
+});
+
+ScrollReveal().reveal(".show-once", {
+    reset: false
+});
+
+ScrollReveal().reveal(".slide-up", {
+    duration: 1000,
+    origin: "bottom",
+    distance: "100px",
+    easing: "ease-in-out"
+});
+
+ScrollReveal().reveal(".flip", {
+    delay: 500,
+    duration: 2000,
+    rotate: {
+        x: 20,
+        z: 20
+    }
+});
+
+ScrollReveal().reveal(".slide-left", {
+    duration: 3000,
+    origin: "right",
+    distance: "300px",
+    easing: "ease-in-out"
+});
